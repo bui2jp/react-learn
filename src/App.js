23f5801a3ce706602f402f709a159a4e0deaedac
takuya <>
2022-02-22 16:02:04 +0900

    props

--- src/App.js
+++ src/App.js
@@ -2,46 +2,22 @@ import React from "react";
 
 //1 Function Component
 function App() {
-  const greeting = "aaa bbb ccc";
 
-  //{}の中にJavascriptが記述できる
-  //const dom = <h1 className="foo">This is JSX {greeting}</h1>;
-  //const dom2 = <input type="text" onClick={() => { console.log('test'); }} />;  
+  const myage=5;
+
+  //propsを渡す
   return (    
       <React.Fragment> 
-        <label htmlFor="bar">test:</label>
-        <input type="text" onClick={() => { console.log('test'); }} />
-        <Dog/>
-        <Dog/>
-        <Dog/>                
+        <Dog name={"aaa"} age={10}/>
+        <Dog name={"bbb"} age={myage}/>
+        <Dog name={"ccc"} age={3}/>
       </React.Fragment>
   );
 }
 
-const Dog = () => {
-  return <div>dog</div>
+//propsを受け取る
+const Dog = (props) => {
+  return <div>dog {props.name} age:{props.age}</div>
 }
-// import React from "react";
-
-// }
-// const App = () => {
-//   return React.createElement(
-//     "div",
-//     null,
-//     "test aaaa this is javascript..."
-//   );
-// }
-
-//2 Class Component
-// import React, { Component } from "react";
-// class App extends Component {
-//   render(){
-//     return React.createElement(
-//       "div",
-//       null,
-//       "test this is from javascript."
-//     );
-//   }
-// }
 
 export default App;

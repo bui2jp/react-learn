a000a8592c806f8cda3d93372c4be6edfb28e0ab
takuya <>
2022-02-22 15:21:08 +0900

    function component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+//1 Function Component
+// function App() {
+//   return (
+//       <h1>This is JSX</h1>
+//   );
 
-function App() {
-  return (
-      <h1>This is JSX</h1>
+import React from "react";
+
+// }
+const App = () => {
+  return React.createElement(
+    "div",
+    null,
+    "test aaaa this is javascript..."
   );
 }
 
+//2 Class Component
 // import React, { Component } from "react";
-
 // class App extends Component {
 //   render(){
 //     return React.createElement(

fdbc33e6c7fcee2b88802d8e2e154a43618bd0bc
takuya <>
2022-02-24 10:14:16 +0900

    redux

---
+++ src/action/index.js
@@ -0,0 +1,18 @@
+//
+// actionを定義 action creator
+//
+
+//typeの定義
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
+
+export const increment = () => {
+    return {
+        type: INCREMENT
+    }
+}
+export const decrement = () => {
+    return {
+        type: DECREMENT
+    }
+}
